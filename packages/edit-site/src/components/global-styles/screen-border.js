a89cbb1aa7c4ba2fcba966add659d2e90f5cfb5f
madhusudhand <madhusudhan.dollu@gmail.com>
2022-12-12 17:43:38 +0530

    add shadow ui controls in global styles

--- packages/edit-site/src/components/global-styles/screen-border.js
+++ packages/edit-site/src/components/global-styles/screen-border.js
@@ -10,10 +10,12 @@ import ScreenHeader from './header';
 import BorderPanel, { useHasBorderPanel } from './border-panel';
 import BlockPreviewPanel from './block-preview-panel';
 import { getVariationClassNameFromPath } from './utils';
+import ShadowPanel, { useHasShadowControl } from './shadow-panel';
 
 function ScreenBorder( { name, variationPath = '' } ) {
 	const hasBorderPanel = useHasBorderPanel( name );
 	const variationClassName = getVariationClassNameFromPath( variationPath );
+	const hasShadowPanel = useHasShadowControl( name );
 	return (
 		<>
 			<ScreenHeader title={ __( 'Border' ) } />
@@ -21,6 +23,7 @@ function ScreenBorder( { name, variationPath = '' } ) {
 			{ hasBorderPanel && (
 				<BorderPanel name={ name } variationPath={ variationPath } />
 			) }
+			{ hasShadowPanel && <ShadowPanel name={ name } /> }
 		</>
 	);
 }
