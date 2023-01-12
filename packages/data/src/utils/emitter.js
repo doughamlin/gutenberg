f8f1b07f9a9cefc926d5320c4c4a94f9f44eed33
Dennis Snell <dennis.snell@automattic.com>
2023-01-11 17:35:09 -0800

    Perf: Avoid cloning listeners in data event emitter
    In the core event emitter class for the `@data` module we have a note
    explaining that we clone the set of listeners to avoid calling newly
    created listeners during the `notifyListeners` loop.
    
    In this patch we use a special token inserted at the end of the listeners
    set that indicates when we've reached the last previously-existing listener
    while iterating. This is a result of `Set` iteration following insertion
    order.
    
    By adding, watching for, and removing this token we can safely iterate
    the set without cloning on every call, which can reduce pressure on the
    garbage collector and lead to lower memory use and reduced GC overhead.
    
    The results of this change may or may not be easily measurable, but if it's
    found to be a positive change regardless it might still be worth introducing
    since this code is in the critical path of all state updates, and it runs
    following every key press.

--- packages/data/src/utils/emitter.js
+++ packages/data/src/utils/emitter.js
@@ -7,11 +7,28 @@ export function createEmitter() {
 	let isPaused = false;
 	let isPending = false;
 	const listeners = new Set();
-	const notifyListeners = () =>
-		// We use Array.from to clone the listeners Set
-		// This ensures that we don't run a listener
-		// that was added as a response to another listener.
-		Array.from( listeners ).forEach( ( listener ) => listener() );
+
+	const notifyListeners = () => {
+		// Some listeners might add new listeners while we're
+		// iterating over the set of existing listeners. We
+		// want to avoid calling them on this loop through the
+		// set. Since Set iteration occurs in order or insertion
+		// we can add a unique symbol to indicate the last existing
+		// listener and stop iterating when we come to it. All
+		// listeners added after the symbol _must_ have been
+		// added while iterating, and we can skip them until
+		// the next time this is called.
+		const existingEndToken = () => {};
+		listeners.add( existingEndToken );
+		for ( const listener of listeners ) {
+			if ( existingEndToken === listener ) {
+				// Stop processing listeners added during this call to notify.
+				break;
+			}
+			listener();
+		}
+		listeners.delete( existingEndToken );
+	};
 
 	return {
 		get isPaused() {
